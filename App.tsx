c4e5c8b73f5b99342ab06cea69666b99ae0a2838
rimul-designe <lumircmolik@gmail.com>
2025-09-09 11:38:45 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
+// App.tsx
+import React, { useEffect } from 'react';
 import * as Notifications from 'expo-notifications';
 import Tabs from './src/navigation/Tabs';
-
-// jak se mají zobrazovat notifikace, když je appka v popředí
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
-});
+import { ensureNotificationSetup } from './src/lib/notifications';
 
 export default function App() {
+  useEffect(() => {
+    // požádáme o oprávnění hned po startu
+    ensureNotificationSetup();
+  }, []);
+
   return <Tabs />;
 }
