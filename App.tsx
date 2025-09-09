35c1837ebc2f9a4b4051c6d2a1f27ff6ece97b7e
rimul-designe <lumircmolik@gmail.com>
2025-09-09 12:02:23 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,8 @@
-// App.tsx
 import React, { useEffect } from 'react';
-import * as Notifications from 'expo-notifications';
 import Tabs from './src/navigation/Tabs';
 import { ensureNotificationSetup } from './src/lib/notifications';
 
 export default function App() {
-  useEffect(() => {
-    // požádáme o oprávnění hned po startu
-    ensureNotificationSetup();
-  }, []);
-
+  useEffect(() => { ensureNotificationSetup(); }, []);
   return <Tabs />;
 }
