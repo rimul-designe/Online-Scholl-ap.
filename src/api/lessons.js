3916575e53be7a64bdb050cd5892389bbaa245ff
rimul-designe <lumircmolik@gmail.com>
2025-09-09 09:40:47 +0200

    Update lessons.js

--- src/api/lessons.js
+++ src/api/lessons.js
@@ -1,6 +1,27 @@
-src/
-  api/lessons.js
-  lib/notifications.js
-  navigation/Tabs.js
-  screens/ScheduleScreen.js
-App.js
+// src/api/lessons.js
+// Falešná data – zatím uložené přímo v souboru
+// Později se to může napojit na backend nebo databázi
+
+export const lessons = [
+  {
+    id: 1,
+    subject: "Matematika",
+    topic: "Algebra – rovnice",
+    time: "08:00",
+    teacher: "Nováková"
+  },
+  {
+    id: 2,
+    subject: "Čeština",
+    topic: "Literatura – Mácha",
+    time: "09:00",
+    teacher: "Svoboda"
+  },
+  {
+    id: 3,
+    subject: "Angličtina",
+    topic: "Slovesa v přítomném čase",
+    time: "10:00",
+    teacher: "Kovář"
+  }
+];
