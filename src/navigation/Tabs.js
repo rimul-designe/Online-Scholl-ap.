579006dd760e22a6aff483c947d9b0d052f41314
rimul-designe <lumircmolik@gmail.com>
2025-09-09 11:36:26 +0200

    Update Tabs.js

--- src/navigation/Tabs.js
+++ src/navigation/Tabs.js
@@ -1,33 +1,27 @@
+// src/navigation/Tabs.js
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Text, View } from 'react-native';
 import ScheduleScreen from '../screens/ScheduleScreen';
 
 const Tab = createBottomTabNavigator();
 
+function Placeholder({ title }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 18 }}>{title}</Text>
+    </View>
+  );
+}
+
 export default function Tabs() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ color, size }) => {
-            let iconName;
-
-            if (route.name === 'Rozvrh') {
-              iconName = 'calendar';
-            } else {
-              iconName = 'home';
-            }
-
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
+      <Tab.Navigator screenOptions={{ headerTitleAlign: 'center' }}>
         <Tab.Screen name="Rozvrh" component={ScheduleScreen} />
+        <Tab.Screen name="Úkoly" children={() => <Placeholder title="Úkoly" />} />
+        <Tab.Screen name="Profil" children={() => <Placeholder title="Profil" />} />
       </Tab.Navigator>
     </NavigationContainer>
   );
